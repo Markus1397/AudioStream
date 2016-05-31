24823410cb5b318271780fbecdffe6af374af85f
MrMiDa <Mike@Michaels-MacBook-Pro.local>
2016-05-31 10:55:36 +0200

    Replaced false version

--- Audio_Test/src/RTPClient.java
+++ Audio_Test/src/RTPClient.java
@@ -7,33 +7,33 @@ public class RTPClient implements ControllerListener, Runnable {
     MediaLocator src;
 
     public static void main(String[] args) {
-        RTPClient rtp = new RTPClient("127.0.0.0");
+        RTPClient rtp = new RTPClient("192.168.2.1");
         Thread t = new Thread(rtp);
         t.start();
 
     }
 
     public RTPClient(String ip) {
- String srcUrl = "rtp://" + ip + ":42050/audio/1";
- DataSink sink;
- src = new MediaLocator(srcUrl);
+        String srcUrl = "rtp://" + ip + ":8888/audio/1";
+        DataSink sink;
+        src = new MediaLocator(srcUrl);
     }
     public void run() {
         try {
-     p = Manager.createPlayer(src);
-     p.addControllerListener(this);
-     p.start();
- } catch(Exception e) {
-     e.printStackTrace();
-     System.exit(1);
- }
+            p = Manager.createPlayer(src);
+            p.addControllerListener(this);
+            p.start();
+        } catch(Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
     public synchronized void controllerUpdate(ControllerEvent evt) {
- if (evt instanceof EndOfMediaEvent) {
-     System.exit(0);
- } else {
-     System.out.println(evt.toString());
- }
+        if (evt instanceof EndOfMediaEvent) {
+            System.exit(0);
+        } else {
+            System.out.println(evt.toString());
+        }
     }
 }  
--- Audio_Test/src/RTPServer.java
+++ Audio_Test/src/RTPServer.java
@@ -14,128 +14,127 @@ public class RTPServer implements ControllerListener, Runnable {
     MediaLocator src;
 
     public static void main (String[] args) {
-        RTPServer rtp = new RTPServer("127.0.0.0", "07.mp3");
+        RTPServer rtp = new RTPServer("192.168.2.1", "07.mp3");
         Thread t = new Thread(rtp);
         t.start();
     }
 
     public RTPServer(String ip, String song) {
-    ipAddress = ip;
-    String srcFile = song;
-    File f = new File(song);
-    if(f.exists()){
+        ipAddress = ip;
+        String srcFile = song;
         src = new MediaLocator("file:" + srcFile);
-        }
+
     }
 
     private void setTrackFormat(Processor p) {
-    // Get the tracks from the processor
-    TrackControl [] tracks = p.getTrackControls();
-    // Do we have atleast one track?
-    if (tracks == null || tracks.length < 1) {
-        System.out.println("Couldn't find tracks in processor");
-        System.exit(1);
-    }
+ // Get the tracks from the processor
+ TrackControl [] tracks = p.getTrackControls();
+ // Do we have atleast one track?
+ if (tracks == null || tracks.length < 1) {
+     System.out.println("Couldn't find tracks in processor");
+     System.exit(1);
+ }
+
  // Set the output content descriptor to RAW_RTP
  // This will limit the supported formats reported from
  // Track.getSupportedFormats to only valid RTP formats.
-    ContentDescriptor cd = new ContentDescriptor(ContentDescriptor.RAW_RTP);
-    p.setContentDescriptor(cd);
+ ContentDescriptor cd = new ContentDescriptor(ContentDescriptor.RAW_RTP);
+ p.setContentDescriptor(cd);
 
-    Format supported[];
-    Format chosen;
-    boolean atLeastOneTrack = false;
+ Format supported[];
+ Format chosen;
+ boolean atLeastOneTrack = false;
 
-    // Program the tracks.
-    for (int i = 0; i < tracks.length; i++) {
-        Format format = tracks[i].getFormat();
-               System.out.println("Trenutni format je " +format.getEncoding());
-        if (tracks[i].isEnabled()) {
-     supported = tracks[i].getSupportedFormats();
-     for (int n = 0; n < supported.length; n++)
-         System.out.println("Supported format: " + supported[n]);
+ // Program the tracks.
+ for (int i = 0; i < tracks.length; i++) {
+     Format format = tracks[i].getFormat();
+            System.out.println(format.getEncoding());
+     if (tracks[i].isEnabled()) {
+  supported = tracks[i].getSupportedFormats();
+  for (int n = 0; n < supported.length; n++)
+      System.out.println("Supported format: " + supported[n]);
 
-     // We've set the output content to the RAW_RTP.
-     // So all the supported formats should work with RTP.
-     // We'll just pick the first one.
+  // We've set the output content to the RAW_RTP.
+  // So all the supported formats should work with RTP.
+  // We'll just pick the first one.
 
-     if (supported.length > 0) {
-         chosen = supported[0]; // this is where I tried changing formats
-         tracks[i].setFormat(chosen);
-         System.err.println("Track " + i + " is set to transmit as: " +chosen);
-         atLeastOneTrack = true;
+  if (supported.length > 0) {
+      chosen = supported[0]; // this is where I tried changing formats
+      tracks[i].setFormat(chosen);
+      System.err.println("Track " + i + " is set to transmit as: " +chosen);
+      atLeastOneTrack = true;
+  } else
+      tracks[i].setEnabled(false);
      } else
-         tracks[i].setEnabled(false);
-        } else
-     tracks[i].setEnabled(false);
+  tracks[i].setEnabled(false);
+ }
     }
-       }
 
-       private void transmit(Processor p) {
-    try {
-        DataSource output = p.getDataOutput();
-        PushBufferDataSource pbds = (PushBufferDataSource) output;
-        RTPManager rtpMgr = RTPManager.newInstance();
-        SessionAddress localAddr, destAddr;
-        SendStream sendStream;
-        int port = 42050;
-        SourceDescription srcDesList[];
-        localAddr = new SessionAddress( InetAddress.getLocalHost(), port);
-        InetAddress ipAddr = InetAddress.getByName(ipAddress);
-        destAddr = new SessionAddress( ipAddr, port);
-        rtpMgr.initialize(localAddr);
-        rtpMgr.addTarget(destAddr);
-        sendStream = rtpMgr.createSendStream(output, 0);
-        sendStream.start();
-        System.err.println( "Created RTP session: " + ipAddress + " " + port);
-        p.start();
-    } catch(Exception e) {
-        e.printStackTrace();
+    private void transmit(Processor p) {
+ try {
+     DataSource output = p.getDataOutput();
+     PushBufferDataSource pbds = (PushBufferDataSource) output;
+     RTPManager rtpMgr = RTPManager.newInstance();
+     SessionAddress localAddr, destAddr;
+     SendStream sendStream;
+     int port = 8888;
+     SourceDescription srcDesList[];
+     localAddr = new SessionAddress( InetAddress.getLocalHost(), port);
+     InetAddress ipAddr = InetAddress.getByName(ipAddress);
+     destAddr = new SessionAddress( ipAddr, port);
+     rtpMgr.initialize(localAddr);
+     rtpMgr.addTarget(destAddr);
+     sendStream = rtpMgr.createSendStream(output, 0);
+     sendStream.start();
+     System.err.println( "Created RTP session: " + ipAddress + " " + port);
+     p.start();
+ } catch(Exception e) {
+     e.printStackTrace();
+ }
     }
-       }
 
-       public synchronized void controllerUpdate(ControllerEvent evt) {
-    if (evt instanceof RealizeCompleteEvent) {
-        realized = true;
-    } else  if (evt instanceof ConfigureCompleteEvent) {
-        configured = true;
-    } else if (evt instanceof EndOfMediaEvent) {
-        System.exit(0);
-    } else {
-        // System.out.println(evt.toString());
+    public synchronized void controllerUpdate(ControllerEvent evt) {
+ if (evt instanceof RealizeCompleteEvent) {
+     realized = true;
+ } else  if (evt instanceof ConfigureCompleteEvent) {
+     configured = true;
+ } else if (evt instanceof EndOfMediaEvent) {
+     System.exit(0);
+ } else {
+     // System.out.println(evt.toString());
+ }
     }
-       }
 
-       public void run() {
+    public void run() {
 
-    try {
-        p = Manager.createProcessor(src);
-        p.addControllerListener(this);
-        p.configure();
-        while (! configured) {
-     try {
-         Thread.currentThread().sleep(100L);;
-     } catch (InterruptedException e) {
-         // ignore
+ try {
+     p = Manager.createProcessor(src);
+     p.addControllerListener(this);
+     p.configure();
+     while (! configured) {
+  try {
+      Thread.currentThread().sleep(100L);;
+  } catch (InterruptedException e) {
+      // ignore
+  }
      }
-        }
 
-        setTrackFormat(p);
-        p.setContentDescriptor(new ContentDescriptor(ContentDescriptor.RAW_RTP));
+     setTrackFormat(p);
+     p.setContentDescriptor(new ContentDescriptor(ContentDescriptor.RAW_RTP));
 
-        p.realize();
-        while (! realized) {
-     try {
-         Thread.currentThread().sleep(100L);;
-     } catch (InterruptedException e) {
-         // ignore
+     p.realize();
+     while (! realized) {
+  try {
+      Thread.currentThread().sleep(100L);;
+  } catch (InterruptedException e) {
+      // ignore
+  }
      }
-        }
-        transmit(p);
+     transmit(p);
 
-    } catch(Exception e) {
-        e.printStackTrace();
-        System.exit(1);
-    }
+ } catch(Exception e) {
+     e.printStackTrace();
+     System.exit(1);
+ }
     }
 }
