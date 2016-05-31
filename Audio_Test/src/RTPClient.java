b5316e46c239034aa25bebcfb6763665f0dd596b
MrMiDa <Mike@Michaels-MacBook-Pro.local>
2016-05-31 10:47:48 +0200

    no message

---
+++ Audio_Test/src/RTPClient.java
@@ -0,0 +1,39 @@
+import javax.media.*;
+
+
+public class RTPClient implements ControllerListener, Runnable {
+
+    Player p;
+    MediaLocator src;
+
+    public static void main(String[] args) {
+        RTPClient rtp = new RTPClient("127.0.0.0");
+        Thread t = new Thread(rtp);
+        t.start();
+
+    }
+
+    public RTPClient(String ip) {
+ String srcUrl = "rtp://" + ip + ":42050/audio/1";
+ DataSink sink;
+ src = new MediaLocator(srcUrl);
+    }
+    public void run() {
+        try {
+     p = Manager.createPlayer(src);
+     p.addControllerListener(this);
+     p.start();
+ } catch(Exception e) {
+     e.printStackTrace();
+     System.exit(1);
+ }
+    }
+
+    public synchronized void controllerUpdate(ControllerEvent evt) {
+ if (evt instanceof EndOfMediaEvent) {
+     System.exit(0);
+ } else {
+     System.out.println(evt.toString());
+ }
+    }
+}  
