9cf86b222500e4f3e31da45433dbbc8c909d2066
MrMiDa <Mike@Michaels-MacBook-Pro.local>
2016-06-07 09:24:39 +0200

    Upload project which should run

--- Audio_Test/src/RTPClient.java
+++ Audio_Test/src/RTPClient.java
@@ -1,4 +1,4 @@
-import javax.media.*;
+/*import javax.media.*;
 
 
 public class RTPClient implements ControllerListener, Runnable {
@@ -36,4 +36,63 @@ public class RTPClient implements ControllerListener, Runnable {
             System.out.println(evt.toString());
         }
     }
-}  
+}  */
+
+/*
+ * This file is part of VLCJ.
+ *
+ * VLCJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VLCJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VLCJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2009-2016 Caprica Software Limited.
+ */
+
+
+
+import uk.co.caprica.vlcj.component.AudioMediaPlayerComponent;
+import uk.co.caprica.vlcj.player.MediaPlayer;
+import uk.co.caprica.vlcj.test.VlcjTest;
+
+/**
+ * The smallest amount of code to play an audio file.
+ * <p>
+ * Audio is simple since there's no video surface to worry about (unless you want the audio
+ * visualisations).
+ */
+public class RTPClient extends VlcjTest {
+
+    public static void main(String[] args) throws Exception {
+        if(args.length != 1) {
+            System.out.println("Specify an MRL to play");
+            System.exit(1);
+        }
+
+        AudioMediaPlayerComponent audioPlayer = new AudioMediaPlayerComponent() {
+            @Override
+            public void finished(MediaPlayer mediaPlayer) {
+                System.exit(0);
+            }
+
+            @Override
+            public void error(MediaPlayer mediaPlayer) {
+                System.out.println("Failed to play media");
+                System.exit(1);
+            }
+        };
+
+        // Play the MRL specified by the first command-line argument
+        audioPlayer.getMediaPlayer().playMedia(args[0]);
+        // Wait forever
+        Thread.currentThread().join();
+    }
+}
